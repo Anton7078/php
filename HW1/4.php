e0f9c1426faea05fe756ab6a769aa6ba3da47e09
Anton7078 <79451063+Anton7078@users.noreply.github.com>
2021-10-09 22:07:48 +0300

    Add files via upload

---
+++ HW1/4.php
@@ -0,0 +1,18 @@
+<?php
+$title = 'Title';
+$chapter = 'HeadName';
+$year = date('Y');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?=$title;?></title>
+    </head>
+    <body>
+        <h1><?=$chapter;?></h1>
+    </body>
+    <footer>
+        <p><?=$year;?></p>
+    </footer>
+</html>
